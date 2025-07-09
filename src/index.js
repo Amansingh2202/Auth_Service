6f99f2344c62dc5b8b50ac30513466bc163be092
Amansingh2202 <789singhaman@gmail.com>
2025-07-09 18:58:25 +0530

    First commit: project skeleton done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express= require('express');
+const app = express();
+
+
+const {PORT}=require('./config/serverConfig');
+
+ const prepareAndStartServer = async () => {
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port :${PORT}`);
+    }   
+    );
+
+   
+}
+prepareAndStartServer();
